cd2a4a382a08990c0dc26fe0c8cfc0661bdd63c0
j2yongjin <yjlee3854@gmail.com>
2018-10-14 10:46:38 +0900

    추가

---
+++ src/main/java/java/basic/reference/MethodReferenceMain.java
@@ -0,0 +1,37 @@
+package java.basic.reference;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class MethodReferenceMain {
+
+    public static void main(String[] args){
+
+        List<String> lists = new ArrayList<>();
+//        Arrays.sort(lists,String::compareToIgnoreCase);
+        lists.forEach(System.out::println);
+
+        lists.forEach(MethodReferenceTypeOne::staticPrint);  // static
+//        lists.forEach(this::equals);
+    }
+
+}
+
+class MethodReference{
+    public void print(String x){
+
+    }
+}
+
+@FunctionalInterface
+interface MethodReferenceTypeOne {
+    public void print(String x);
+    default void printOne(String x){
+
+    }
+    static void staticPrint(String x){
+
+    }
+
+}
---
+++ src/main/java/modernjava/lamda/PredicateMain.java
@@ -0,0 +1,14 @@
+package modernjava.lamda;
+
+import java.util.function.Predicate;
+
+public class PredicateMain {
+
+    public static void main(String[] args){
+
+        Object a= null;
+        Predicate.isEqual(a).and(Predicate.isEqual(a))
+        .and(Predicate.isEqual(a).or(Predicate.isEqual(a)));
+
+    }
+}
